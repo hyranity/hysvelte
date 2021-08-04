1074dd136ea8108c63eeb526e61cfb7c59dc0459
Johann Lee <mast3r1sgr3at@hotmail.com>
2021-08-04 18:09:43 +0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Router, Route, Link } from "svelte-navigator";
+	import Landing from "./pages/Landing.svelte";
+	
+</script>
+
+<Router primary={false}>
+	<div class="main">
+	  <Route path='/'>
+		<Landing/>
+	  </Route>
+	</div>
+  </Router>
---
+++ src/pages/Second.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { Router, Route, Link } from "svelte-navigator";
+</script>
+
+<Route path="/second">
+    <h1>This is the second page</h1>
+</Route>
